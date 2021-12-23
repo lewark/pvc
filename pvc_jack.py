a24304089f89fff5bb9900fbf2de2d3a089c3f15
knector01 <78086132+knector01@users.noreply.github.com>
2021-12-23 12:16:01 -0700

    Implement circular buffer

--- pvc_jack.py
+++ pvc_jack.py
@@ -5,6 +5,83 @@ import jack
 
 import pvc
 
+class CircularBuffer:
+    def __init__(self, n):
+        self.arr = np.zeros(n)
+        self.widx = 0
+        self.ridx = 0
+        
+    def peek(self, n, offset=0):
+        start = (self.ridx + offset) % self.arr.size
+        end = start + n
+        
+        if end > self.arr.size:
+            l_start = start
+            l_end = self.arr.size
+            r_start = 0
+            r_end = end - self.arr.size
+            
+            split = l_end - l_start
+            
+            out = np.zeros(n)
+            
+            out[:split] = self.arr[l_start:l_end]
+            out[split:] = self.arr[r_start:r_end]
+            
+            return out
+        else:
+            return self.arr[start:end]
+        
+    def write(self, data, offset=0):
+        n = data.size
+        
+        start = (self.widx + offset) % self.arr.size
+        end = start + n
+        
+        if end > self.arr.size:
+            l_start = start
+            l_end = self.arr.size
+            r_start = 0
+            r_end = end - self.arr.size
+            
+            split = l_end - l_start
+            
+            self.arr[l_start:l_end] = data[:split] 
+            self.arr[r_start:r_end] = data[split:]
+        else:
+            self.arr[start:end] = data
+            
+    def write_add(self, data, offset=0):
+        n = data.size
+        
+        start = (self.widx + offset) % self.arr.size
+        end = start + n
+        
+        if end > self.arr.size:
+            l_start = start
+            l_end = self.arr.size
+            r_start = 0
+            r_end = end - self.arr.size
+            
+            split = l_end - l_start
+            
+            self.arr[l_start:l_end] += data[:split] 
+            self.arr[r_start:r_end] += data[split:]
+        else:
+            self.arr[start:end] += data
+        
+    def radvance(self, n):
+        self.ridx = (self.ridx + n) % self.arr.size
+        
+    def wadvance(self, n):
+        self.widx = (self.widx + n) % self.arr.size
+        
+    def get_unread(self):
+        n = self.widx - self.ridx
+        if n < 0:
+            n += self.arr.size
+        return n
+        
 
 class PVCJack:
     def __init__(self):
@@ -14,84 +91,40 @@ class PVCJack:
         self.event = threading.Event()
         self.window_size = 4096
         self.n_windows = 4
+        
+        self.gain = 2
 
-        self.buffer = []
-        self.buffer_readpos = 0
-        self.buffer_samples = 0
+        self.inbuffer = CircularBuffer(self.window_size * 2)
+        self.outbuffer = CircularBuffer(self.window_size * 2)
+        # NOTE: this might not work when window size isn't even multiple of JACK block size
+        self.outbuffer.widx = self.outbuffer.arr.size // 2
 
-        initial_delay = self.window_size * 2
-        self.outbuffer = [
-            np.zeros(self.client.blocksize)
-            for i in range(int(np.ceil(initial_delay / self.client.blocksize)))
-        ]
-        self.outbuffer_writepos = initial_delay
-
-        self.pvc = pvc.PitchShifter(self.client.samplerate, self.window_size, 1, 1, True, 8)
+        self.pvc = pvc.PitchShifter(self.client.samplerate, self.window_size, 2, 1, True, 8)
         
         self.client.set_process_callback(self.process)
         self.client.set_shutdown_callback(self.shutdown)
 
     def process(self, frames):
-        """Abandon hope, all ye who enter here"""
-        #print("process")
         assert frames == self.client.blocksize
         data = self.client.inports[0].get_array()
-        self.buffer.append(np.array(data))
-        self.buffer_samples += frames
+        self.inbuffer.write(data)
+        self.inbuffer.wadvance(frames)
 
-        while self.buffer_samples - self.buffer_readpos >= self.window_size:
-            #print("perform pvc")
-            block = np.zeros(self.window_size)
-            i = 0
-            read = 0
-            delay = self.buffer_readpos
-            #print('begin read')
-            while read < self.window_size:
-                piece = self.buffer[i]
-                if i == 0:
-                    piece = piece[self.buffer_readpos :]
-                n = min(piece.size, self.window_size - i)
-                block[i : i + n] = piece[:n]
-                read += n
-                i += 1
-                #print(i, n, piece.size, self.window_size)
-            #print('finish reading')
-
+        while self.inbuffer.get_unread() >= self.window_size:
             advance = self.window_size // self.n_windows
+
+            block = self.inbuffer.peek(self.window_size)
 
             out_block = self.pvc.process(block, advance, advance)
 
-            i = 0
-            written = 0
-            delay = self.outbuffer_writepos
-            while written < self.window_size:
-                while len(self.outbuffer) < i + 1:
-                    self.outbuffer.append(np.zeros(frames))
-
-                piece = self.outbuffer[i]
-                if delay < piece.size:
-                    to_write = min(piece.size - delay, out_block.size - written)
-                    piece[delay : delay + to_write] += out_block[written : written + to_write]
-                    delay = 0
-                    written += to_write
-                else:
-                    delay -= piece.size
-                #print('writing')
-
-                i += 1
+            self.outbuffer.write(np.zeros(advance), advance * (self.n_windows - 1))
+            self.outbuffer.write_add(out_block)
                 
-            self.buffer_readpos += advance
-            self.outbuffer_writepos += advance
-        #print("finish")
+            self.inbuffer.radvance(advance)
+            self.outbuffer.wadvance(advance)
 
-        while len(self.buffer) > 0 and self.buffer_readpos > self.buffer[0].size:
-            self.buffer_readpos -= self.buffer[0].size
-            self.buffer_samples -= self.buffer[0].size
-            del self.buffer[0]
-
-        # TODO: This breaks if the server buffer size changes
-        self.client.outports[0].get_array()[:] = self.outbuffer.pop(0)
-        self.outbuffer_writepos -= frames
+        self.client.outports[0].get_array()[:] = self.outbuffer.peek(frames) * self.gain
+        self.outbuffer.radvance(frames)
 
     def shutdown(self, status, reason):
         print("JACK shutdown!")
